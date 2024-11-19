de7ae0e3d16602e6617230ef7885be85974492cf
Juan <saraviamartin16@hotmail.com>
2024-11-19 17:58:00 -0300

    profile pic

--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -37,7 +37,7 @@
         </a>
         </div>
         <div class="relative shadow-2xl grid place-items-center">
-            <img src={"images/profile.jpeg"} alt="Profile image" class="object-cover z-[2] max-h-[70vh]"/>
+            <img src={"images/profile.png"} alt="Profile image" class="object-cover z-[2] max-h-[70vh]"/>
         </div>
     </section>
 
